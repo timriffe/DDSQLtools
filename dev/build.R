4d9074e5802a0fc8a0a06ebd30229b6a55bc94b6
Tim Riffe <tim.riffe@gmail.com>
2018-07-19 11:00:25 +0200

    add setwd header

--- dev/build.R
+++ dev/build.R
@@ -1,13 +1,20 @@
 library(devtools)
+
+# MP, you can do something like this so it works for both of us
+if (system("whoami",intern=TRUE) == "tim"){
+	# if I'm on the laptop
+	setwd("/home/tim/git/DDSQLtools")
+} 
+
 #install_github("hadley/devtools")
 # do this whenever new functions are added to /R, or whenever roxygen is updated
-document("/home/tim/git/DDSQLtools")
+document()
 
 # run this to get access to already-written functions
-load_all("/home/tim/git/DDSQLtools")
+load_all()
 
 # do this whenever major changes happen
-check("/home/tim/git/DDSQLtools")
+check()
 
 # try github install
 install_github("timriffe/DDSQLtools")
