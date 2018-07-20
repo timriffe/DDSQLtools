a4f4920415de448ca5423457de2c2d940d64b0ad
mpascariu <xpascariu@outlook.com>
2018-07-20 15:34:59 +0200

    Add wrap function for DemoTools::beers, grabill and sprague. The function knows how to extract values for `popmat`, `Age` and `OAG`. Other argumets must be specified with `...`.
    Add a small data set.

---
+++ R/data_and_imports.R
@@ -0,0 +1,16 @@
+
+#' Here we list all the imports
+#' @import DemoTools
+#' @importFrom tidyr spread gather
+#' @importFrom dplyr select mutate %>%
+#' @importFrom tibble as.tibble
+#' @name foo_imports
+#' @keywords internal
+NULL
+
+
+
+#' UN data - Egypt 1976 - Population by age and sex - abridged
+#'
+#' @source United Nations -- DemoData_Export_Pop5_Egypt+India_DB.xlsx
+"P5_Egypt1976"
