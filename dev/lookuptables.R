027aabfd6180ab2357e36aed43e147ec1e63607f
Tim Riffe <tim.riffe@gmail.com>
2018-11-08 18:09:50 +0100

    edits for new api

--- dev/lookuptables.R
+++ dev/lookuptables.R
@@ -8,7 +8,7 @@
 library(rjson)
 library(reshape2)
 # url from Patrick
-this.url <- "https://popdiv.dfs.un.org/demodata/api/country?addDefault=false&includeDependencies=false&includeFormerCountries=false"
+this.url <- "http://24.239.36.16:9654/un2/api/country?addDefault=false&includeDependencies=false&includeFormerCountries=false"
 Clist <- fromJSON(file = this.url)
 # get tidy data.frame
 Countries <- do.call("rbind",lapply(Clist, as.data.frame))
@@ -22,3 +22,14 @@ Indicators <- do.call("rbind",lapply(Ilist, as.data.frame))
 head(Indicators)
 
 # however other urls have not been so easy with trial and error, many other code lists needed
+
+this.url <-"http://24.239.36.16:9654/un2/api/recordDataDetail?dataProcess=2&indicatorType=8&isComplete=0&loc=4&locAreaType=2&subGroup=2"
+
+X <- fromJSON(file = this.url)
+library(httr)
+install.packages("jsonlite", repos="http://cran.r-project.org")
+X <- jsonlite::fromJSON(this.url)
+
+
+
+
