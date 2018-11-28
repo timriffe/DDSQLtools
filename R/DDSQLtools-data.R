1a9aa7acbc4f346b66ea72346c15e65efcc906cf
Marius Pascariu <rpascariu@outlook.com>
2018-11-28 16:13:59 +0100

    update DDSQLtools.data descritpion and version number in README

--- R/DDSQLtools-data.R
+++ R/DDSQLtools-data.R
@@ -1,7 +1,7 @@
 # --------------------------------------------------- #
 # Author: Marius D. Pascariu
 # License: CC-BY-NC 4.0
-# Last update: Tue Nov 27 20:59:41 2018
+# Last update: Wed Nov 28 16:08:29 2018
 # --------------------------------------------------- #
 
 #' DDSQLtools test data - Egypt 1976 - Population by age and sex
@@ -9,6 +9,7 @@
 #' @details Check \code{dev/AddDataInPackage.R} to see how the data was selected.
 #' @source 
 #' UN Excel files: 
-#' \code{DemoData_Export_Pop1_Egypt_DB.xlsx} and 
-#' \code{DemoData_Export_Pop5_Egypt_DB.xlsx}. 
+#' \code{DemoData_Export_Pop1_Egypt_DB.xlsx}, 
+#' \code{DemoData_Export_Pop5_Egypt_DB.xlsx} and 
+#' \code{LTabr_error1.csv}. 
 "DDSQLtools.data"
--- R/ExtrapolationMethods.R
+++ R/ExtrapolationMethods.R
@@ -79,5 +79,3 @@ doExtrapolate <- function(X,
 }
 
 
-
-
