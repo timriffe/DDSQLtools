61f5b7884d0719ce28297f217f72ffa54a613795
Marius Pascariu <rpascariu@outlook.com>
2018-12-13 17:12:30 +0100

    UN Data Browser: add linkGenerator() function. It may prove useful for undertanding the process (later on). + More of "do not repeat yourself".

--- R/GetInformation.R
+++ R/GetInformation.R
@@ -1,12 +1,12 @@
 # --------------------------------------------------- #
 # Author: Marius D. Pascariu
 # License: CC-BY-NC 4.0
-# Last update: Thu Dec 13 16:17:30 2018
+# Last update: Thu Dec 13 17:07:36 2018
 # --------------------------------------------------- #
 
 
 #' Get information about available locations (LocID)
-#' @inheritParams read_API_a
+#' @inheritParams read_API
 #' @examples 
 #' L <- getLocations(addDefault = "false",
 #'                   includeDependencies = "false",
@@ -15,27 +15,27 @@
 #' @export
 getLocations <- function(save = FALSE, ...) {
   
-  read_API_a("country", save, ...)
+  read_API("country", save, ...)
 }
 
 
 #' Get information about available location types (LocAreaTypeID)
-#' @inheritParams read_API_a
+#' @inheritParams read_API
 #' @examples 
 #' # Check what subgroups are available for:
-#' P <- getLocationTypes(indicatorType = 8,  # Population by age and sex indicator;
-#'                       loc = 818,          # Egypt
+#' P <- getLocationTypes(indicatorType = 8, # Population by age and sex indicator;
+#'                       loc = 818,         # Egypt
 #'                       isComplete = 0)
 #' P
 #' @export
 getLocationTypes <- function(save = FALSE, ...) {
   
-  read_API_a("locationType", save, ...)
+  read_API("locationType", save, ...)
 }
 
 
 #' Get information about available sub-group-types (SubGroupTypeID)
-#' @inheritParams read_API_a
+#' @inheritParams read_API
 #' @examples 
 #' # Check what subgroups are available for:
 #' S <- getSubGroups(indicatorType = 8,  # Population by age and sex indicator;
@@ -45,108 +45,60 @@ getLocationTypes <- function(save = FALSE, ...) {
 #' @export
 getSubGroups <- function(save = FALSE, ...) {
   
-  read_API_a("subGroup", save, ...)
+  read_API("subGroup", save, ...)
 }
 
 
 #' Get information about available indicators (IndicatorTypeID)
-#' @inheritParams read_API_b
+#' @inheritParams read_API
 #' @examples 
 #' I <- getIndicators(addDefault = "false")
 #' I[, c("IndicatorTypeID", "Name", "ShortName")]
 #' @export
 getIndicators <- function(save = FALSE, ...) {
   
-  read_API_b("Indicator", save, ...)
+  read_API("Indicator", save, ...)
 }
 
 
 #' Get information about available data-types (DataProcessTypeID)
-#' @inheritParams read_API_b
+#' @inheritParams read_API
 #' @examples 
 #' D <- getDataProcessTypes()
 #' D
 #' @export
 getDataProcessTypes <- function(save = FALSE, ...) {
   
-  read_API_b("dataProcessTypes", save, ...)
+  read_API("dataProcessTypes", save, ...)
 }
 
 
-#' Download data - version A
-#' @param this_table Type of data;
-#' @param server The path to the database. Default: 
-#' \code{"http://24.239.36.16:9654/un2/api/"};
+#' Download data
 #' @param save Logical. Choose whether or not to save the data in an external 
 #' \code{.Rdata} file in the working directory. Default: 
 #' \code{FALSE};
-#' @param ... Other arguments that might define the path to data. Handle with 
-#' care, this is important! The following options are available: \itemize{
-#'   \item{\code{dataProcess}} -- Data process ID as defined by the UNPD. 
-#'   Run the \code{\link{getDataProcessTypes}} function to see the available 
-#'   options;
-#'   \item{\code{startYear}} -- Start year. Default: NULL;
-#'   \item{\code{endYear}} -- End year. Default: NULL;
-#'   \item{\code{indicatorType}} -- Indicator type ID as defined by the UNPD. 
-#'   Run the \code{\link{getIndicators}} function to see the available options;
-#'   \item{\code{isComplete}} -- isComplete;
-#'   \item{\code{loc}} -- Location ID as defined by the UNPD. Run the
-#'   \code{\link{getLocations}} function to see the available options;
-#'   \item{\code{locAreaType}} -- Location area type ID as defined by the UNPD. 
-#'   Run the \code{\link{getLocationTypes}} function to see the available 
-#'   options;
-#'   \item{\code{subGroup}} -- SubGroup ID as defined by the UNPD. Run the 
-#'   \code{\link{getSubGroups}} function to see the available options;
-#'   \item{\code{addDefault}} -- Logical. Default: FALSE;
-#'   \item{\code{includeDependencies}} -- Logical. Default: FALSE;
-#'   \item{\code{includeFormerCountries}} -- Logical. Default: FALSE.
-#'   }
+#' @inheritParams linkGenerator
 #' @keywords internal
-read_API_a <-function(this_table, 
-                      save, 
-                      server = "http://24.239.36.16:9654/un2/api/", 
-                      ...) {
+read_API <-function(type, 
+                    save, 
+                    ...) {
   
   . <- NULL # hack CRAN check note
+  P <- linkGenerator(type = type, ...) # path
+  X <- fromJSON(file = P)
   
-  this_data <- build_filter(...)
-  this_path <- paste0(server, this_table, this_data)
+  if (type %in% c("Indicator", "dataProcessTypes")) {
+    X <- X %>% lapply(unlist) 
+  }
   
-  X <- fromJSON(file = this_path)
-  out <- suppressWarnings(X %>% 
-                            lapply(unlist) %>% 
-                            do.call("rbind", .) %>% 
-                            as.data.frame)
+  Z <- suppressWarnings(X %>% do.call("rbind", .) %>% as.data.frame)
   # Some of the lists might have different number of elements. When we rbind
   # a warning is displayed. We suppress it here. It seems to work fine.
   
-  if (save) save_in_working_dir(data = out, 
-                                file_name = paste0("UNPD_", this_table))
-  return(out)
+  if (save) save_in_working_dir(data = Z, 
+                                file_name = paste0("UNPD_", type))
+  return(Z)
 }
 
-
-#' Download data - version B
-#' @inheritParams read_API_a
-#' @keywords internal
-read_API_b <-function(this_table, 
-                      save, 
-                      server = "http://24.239.36.16:9654/un2/api/", 
-                      ...) {
-  
-  . <- NULL # hack CRAN check note
-  
-  this_data <- if (this_table == "dataProcessTypes") "All" else build_filter(...)
-  this_path <- paste0(server, this_table, this_data)
-  
-  X <- fromJSON(file = this_path)
-  out <- suppressWarnings(X %>% 
-                            do.call("rbind", .) %>% 
-                            as.data.frame)
-  
-  if (save) save_in_working_dir(data = out, 
-                                file_name = paste0("UNPD_", this_table))
-  return(out)
-}
 
 
--- R/Utils_Gets.R
+++ R/Utils_Gets.R
@@ -1,22 +1,79 @@
 # --------------------------------------------------- #
 # Author: Marius D. Pascariu
 # License: CC-BY-NC 4.0
-# Last update: Thu Dec 13 15:20:55 2018
+# Last update: Thu Dec 13 17:07:27 2018
 # --------------------------------------------------- #
 
-
-#' Format data from character to numeric
-#' @description When a data is downloaded from web it is saved as a list or 
-#' data.frame with columns containing strings of information (character format).
-#' This function reads the values and if it sees in these columns only numbers
-#' will convert the column to class numeric.
-#' @param A data.frame or matrix.
-#' @keywords internal
-format.numeric.colums <- function(X) {
-  cn <- colnames(X) 
-  isNum <- apply(X, 2, FUN = function(w) w %>% check.numeric %>% all)
-  out <- data.frame(X[, !isNum], apply(X[, isNum], 2, as.numeric))
-  out[, cn]
+#' API Link Generator Function
+#' @param server The path to the database. Default: 
+#' \code{"http://24.239.36.16:9654/un2/api/"};
+#' @param type Type of data. Various options are available.
+#' @param ... Other arguments that might define the path to data. Handle with 
+#' care, this is important! The following options are available: \itemize{
+#'   \item{\code{dataProcess}} -- Data process ID as defined by the UNPD. 
+#'   Run the \code{\link{getDataProcessTypes}} function to see the available 
+#'   options;
+#'   \item{\code{startYear}} -- Start year. Default: NULL;
+#'   \item{\code{endYear}} -- End year. Default: NULL;
+#'   \item{\code{indicatorType}} -- Indicator type ID as defined by the UNPD. 
+#'   Run the \code{\link{getIndicators}} function to see the available options;
+#'   \item{\code{isComplete}} -- isComplete;
+#'   \item{\code{loc}} -- Location ID as defined by the UNPD. Run the
+#'   \code{\link{getLocations}} function to see the available options;
+#'   \item{\code{locAreaType}} -- Location area type ID as defined by the UNPD. 
+#'   Run the \code{\link{getLocationTypes}} function to see the available 
+#'   options;
+#'   \item{\code{subGroup}} -- SubGroup ID as defined by the UNPD. Run the 
+#'   \code{\link{getSubGroups}} function to see the available options;
+#'   \item{\code{addDefault}} -- Logical. Default: FALSE;
+#'   \item{\code{includeDependencies}} -- Logical. Default: FALSE;
+#'   \item{\code{includeFormerCountries}} -- Logical. Default: FALSE.
+#'   }
+#' @details The link generator is based on the structure of the database 
+#' created by Dennis Butler (in late 2018). If the web address or the structure 
+#' of the database changes this will have to be updated as well.
+#' @examples 
+#' # Link to country list
+#' L1 <- linkGenerator(type = "country",
+#'                     addDefault = "false",
+#'                     includeDependencies = "false",
+#'                     includeFormerCountries = "false")
+#' L1
+#' 
+#' # Link to location types (for Egypt)
+#' L2 <- linkGenerator(type = "locationType",
+#'                     indicatorType = 8,
+#'                     loc = 818,
+#'                     isComplete = 0)
+#' L2
+#' 
+#' # Link to subgroup types (for Egypt)
+#' L3 <- linkGenerator(type = "subGroup",
+#'                     indicatorType = 8,
+#'                     loc = 818,
+#'                     isComplete = 0)
+#' L3
+#' 
+#' # Link to indicator list
+#' L4 <- linkGenerator(type = "Indicator",
+#'                     addDefault = "false")
+#' L4
+#' 
+#' # Link to data process type list
+#' L5 <- linkGenerator(type = "dataProcessTypes")
+#' L5
+#' @export
+linkGenerator <- function(server = "http://24.239.36.16:9654/un2/api/", 
+                          type = c("country", 
+                                   "locationType", 
+                                   "subGroup",
+                                   "Indicator", 
+                                   "dataProcessTypes"),
+                          ...) {
+  
+  type <- match.arg(type)
+  this_data <- if (type == "dataProcessTypes") "All" else build_filter(...)
+  paste0(server, type, this_data)
 }
 
 
@@ -65,6 +122,22 @@ build_filter <- function(dataProcess = NULL,
 }
 
 
+
+#' Format data from character to numeric
+#' @description When a data is downloaded from web it is saved as a list or 
+#' data.frame with columns containing strings of information (character format).
+#' This function reads the values and if it sees in these columns only numbers
+#' will convert the column to class numeric.
+#' @param A data.frame or matrix.
+#' @keywords internal
+format.numeric.colums <- function(X) {
+  cn <- colnames(X) 
+  isNum <- apply(X, 2, FUN = function(w) w %>% check.numeric %>% all)
+  out <- data.frame(X[, !isNum], apply(X[, isNum], 2, as.numeric))
+  out[, cn]
+}
+
+
 #' Save downloaded data in a .Rdata file located in the working directory
 #' @param data The dataset to be saved;
 #' @param file_name Name to be assined to the data.
@@ -80,5 +153,6 @@ save_in_working_dir <- function(data, file_name) {
           appendLF = FALSE)
   cat("\n   ")
 }
+
 
 
