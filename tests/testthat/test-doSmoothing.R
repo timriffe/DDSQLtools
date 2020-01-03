0ae89c4fe98d0fd4f77d7f24df36c1e7bf1bbdfa
Jorge Cimentada <cimentadaj@gmail.com>
2020-01-03 13:34:48 +0100

    Revamps doSmoothing tests

--- tests/testthat/test-doSmoothing.R
+++ tests/testthat/test-doSmoothing.R
@@ -1,27 +1,25 @@
-## DATA
+# Data
 I <- DDSQLtools.data$Pop5_Egypt_M_DB
 
-# ------------------------------------------
-## TEST Design
 
-#' @param I Input
-#' @param O Output
 validate_smoothing_method <- function(I, O) {
-  test_that("Test smoothing methods", {
-    expect_output(print(O))                      # 1. We always expect an output;
-    expect_true(any(class(O) == "data.frame"))   # 2. Output should be a data.frame;
-    expect_identical(dim(O), dim(I))             # 3. Output dimensions are identical with input dimensions
-    expect_true(is.numeric(O$DataValue))         # 4. DataValue is numeric;
-    expect_true(all(O$DataValue >= 0))           # 5. And all positive;
-    expect_identical(O$AgeStart, I$AgeStart)     # 6. Same AgeStart as in input
-    expect_identical(O$AgeEnd,   I$AgeEnd)       # 7. Same AgeEnd as in input
-    expect_identical(O$AgeLabel, I$AgeLabel)     # 8. Same AgeLabel as in input
-  })
-  
+    # 1. We always expect an output;
+    expect_output(print(O))
+    # 2. Output should be a data.frame;
+    expect_true(any(class(O) == "data.frame"))
+    # 3. Output dimensions are identical with input dimensions
+    expect_identical(dim(O), dim(I))
+    # 4. DataValue is numeric;
+    expect_true(is.numeric(O$DataValue))
+    # 5. And all positive;
+    expect_true(all(O$DataValue >= 0))
+    # 6. Same AgeStart as in input
+    expect_identical(O$AgeStart, I$AgeStart)
+    # 7. Same AgeEnd as in input
+    expect_identical(O$AgeEnd,   I$AgeEnd)
+    # 8. Same AgeLabel as in input
+    expect_identical(O$AgeLabel, I$AgeLabel)
 }
-
-# ------------------------------------------
-## We run the tests here:
 
 for (i in c("Carrier-Farrag", 
             "KKN", 
@@ -30,10 +28,10 @@ for (i in c("Carrier-Farrag",
             "Strong", 
             "Zigzag", 
             "MAV")) { 
-  
-  cat("Test", i, "method: ")
-  O <- doSmoothing(X = I, method = i)
-  validate_smoothing_method(I, O)       # Validate.
-  cat("OK\n")
+
+  test_that(paste0("doSmoothing with ", i), {
+    O <- doSmoothing(X = I, method = i)
+    validate_smoothing_method(I, O)
+  })
 }
 
