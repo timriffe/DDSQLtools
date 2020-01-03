50a2f9427b8162f3b8a4636f686805a6ac273f9a
Jorge Cimentada <cimentadaj@gmail.com>
2020-01-03 13:06:47 +0100

    Revamp doExtrapolate test file

--- tests/testthat/test-doExtrapolate.R
+++ tests/testthat/test-doExtrapolate.R
@@ -1,4 +1,4 @@
-## DATA
+# Data for comparison
 Mx5 <- DDSQLtools.data$Mx5 
 x1  <- c(75, 80, 85, 90)        # ages to be used in fitting
 x2  <- c(90, 95, 100, 105, 110) # ages for which the extrapolation is applied
@@ -12,37 +12,40 @@ M <- c("kannisto",              # Models
        "quadratic")
 
 
-# ------------------------------------------
-## TEST Design
-
-#' @param I Input data
-#' @param O Output data
-validate_here <- function(I, O) {
+validate_data <- function(I, O) {
   test_that("Test Wrapper Output", {
-    expect_true(all(I$DataValue >= 0))           # 1. The input should be positive;
-    expect_output(print(O))                      # 2. We always expect an output;
-    expect_true(any(class(O) == "data.frame"))   # 3. Output should be a data.frame;
-    # expect_true(ncol(O) == ncol(I))            # 4. No of columns as in input;
-    expect_true(nrow(O) >= nrow(I))              # 5. The output must have the same or more rows as the input;
-    expect_true(class(O$DataValue) == "numeric") # 6. DataValue is always numeric;
-    expect_true(all(O$DataValue >= 0))           # 7. And positive;
-    expect_true(all(O$DataValue <= 1e2))         # 8. If the value in DataValue is to large maybe somebody should come back here and do a manual inspection.
+    # 1. The input should be positive;
+    expect_true(all(I$DataValue >= 0))
+    # 2. We always expect an output;
+    expect_output(print(O))
+    # 3. Output should be a data.frame;
+    expect_true(any(class(O) == "data.frame"))
+    # 4. No of columns as in input;
+    # expect_true(ncol(O) == ncol(I))
+    # 5. The output must have the same or more rows as the input;
+    expect_true(nrow(O) >= nrow(I))
+    # 6. DataValue is always numeric;
+    expect_true(class(O$DataValue) == "numeric")
+    # 7. And positive;
+    expect_true(all(O$DataValue >= 0))
+    # 8. If the value in DataValue is to large maybe somebody should come back
+    # here and do a manual inspection.
+    expect_true(all(O$DataValue <= 1e2))
   })
   
 }
 
+for (i in M) {
+  test_that(paste0("doExtrapolate with ", i), {
+    # Expect a message here;
+    expect_message(
+      E1 <- doExtrapolate(X = Mx5,
+                          x_fit = x1,
+                          x_extr = x2,
+                          law = i,
+                          verbose = TRUE)
+    )
 
-
-# ------------------------------------------
-## We run the tests here:
-
-for (i in M) { 
-  cat("Test", i, "method: ")
-  expect_message(E1 <- doExtrapolate(X = Mx5, 
-                                     x_fit = x1, 
-                                     x_extr = x2, 
-                                     law = i,
-                                     verbose = TRUE)) # Expect a message here;
-  validate_here(I = Mx5, O = E1)                      # Validate;
-  cat("OK\n")
+    validate_data(I = Mx5, O = E1)
+  })
 }
